70050fcd42427c509129d33a4bd18c852aabfd5a
we_are_but_neutrinos <adithyasudhan@gmail.com>
2023-03-09 14:44:23 +0000

    Update 18 files

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,15 +1,16 @@
-import { Box,chakra } from "@chakra-ui/react";
+import { Box, chakra } from "@chakra-ui/react";
 export default function Card() {
   return (
-    <Box textAlign={"center"} width={"100%"} margin={'auto'}>
-        <chakra.h1
+    <Box textAlign={"center"} width={"100%"} margin={"auto"}>
+      <chakra.h1
         py={5}
         fontSize={48}
-        fontFamily={'Work Sans'}
-        fontWeight={'bold'}
-        color={"gray.800"}>
+        fontFamily={"Work Sans"}
+        fontWeight={"bold"}
+        color={"gray.800"}
+      >
         My Quizzes
-        </chakra.h1>
+      </chakra.h1>
     </Box>
   );
 }
