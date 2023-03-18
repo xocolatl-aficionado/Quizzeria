e01ef16bf9e34735f8933f41627217af57d935d5
Mehadi Hassan <mehadihassan116@gmail.com>
2023-03-18 12:00:48 -0230

    Added User Session

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -8,32 +8,55 @@ import Footer  from '../../components/Footer'
 import Card  from '../../components/Card'
 import TableView  from '../../components/adminHomeTable'
 import { Box } from "@chakra-ui/react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 /**
  * Function to buid the admin Hompe page with components
  * @returns the home page for admins build with the components Navigation Bar, Title Card, a table containing the admin's quizzes and the footer. 
  */
 function adminHome() {
-    return (
-        <>
-          <Head>
-            <title>Quiz App</title>
-            <meta name="description" content="Quiz App Home for Admin" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-          <ChakraProvider>
-            <Box minHeight="100vh" display="flex" flexDirection="column">
-                <NavBar />
-                <Box justifyContent="center"><Card/></Box>
-                <Box flex="1" width='80%'mx="auto" justifyContent="center">
-                    <TableView/>
-                </Box>
-                <Footer />
-            </Box>
-          </ChakraProvider>
-        </>
-      )
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  const userRole = (session: any) => {
+    let role = session?.user?.role;
+    if (role) return role;
+    return null;
+  };
+
+  useEffect (() => {
+    if (status === "unauthenticated") router.replace ("/");
+    }, [status]);
+  
+    if (status === "authenticated"){
+      var role: any = userRole(session);
+      if (role == "student") {
+        router.replace("/studnet");
+      } else if (role == "admin") {
+      return (
+          <>
+            <Head>
+              <title>Quiz App</title>
+              <meta name="description" content="Quiz App Home for Admin" />
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <ChakraProvider>
+              <Box minHeight="100vh" display="flex" flexDirection="column">
+                  <NavBar />
+                  <Box justifyContent="center"><Card/></Box>
+                  <Box flex="1" width='80%'mx="auto" justifyContent="center">
+                      <TableView/>
+                  </Box>
+                  <Footer />
+              </Box>
+            </ChakraProvider>
+          </>
+        )
+      }
+    }
   }
   
   export default adminHome;
