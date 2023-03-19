2d2d4042cd0c37c8eee4276bf090e6b3b0057e4f
Heshani2023 <hhettiarachc@mun.ca>
2023-03-19 20:23:44 -0230

    Implemented  QuizBank  Page for students

--- pages/student/quizBank/index.tsx
+++ pages/student/quizBank/index.tsx
@@ -1,12 +1,11 @@
-/**
- * Import required reusable components for creating the quizbank UI
- */
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "../../../components/studentNavbar";
 import Footer from "../../../components/Footer";
 import Card from "../../../components/QuizBankTitle";
 import QuizBankView from "../../../components/QuizBankTable";
+import Card from "../../../components/Card";
+import QuizTable from "../../../components/pagination";
 import { Box } from "@chakra-ui/react";
 import clientPromise from "../../../src/lib/mongodb";
 
@@ -27,10 +26,6 @@ export async function getServerSideProps() {
     console.error(e);
   }
 }
-/**
- * Creating the QuizBank UI using reusable components
- * @returns 
- */
 export default function QuizBank({ quizzes }) {
   return (
     <>
@@ -39,6 +34,7 @@ export default function QuizBank({ quizzes }) {
         <meta name="description" content="Quiz App Home for students" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="../../../components/styles.css" />
       </Head>
       <ChakraProvider>
         <Box minHeight="100vh" display="flex" flexDirection="column">
@@ -48,6 +44,8 @@ export default function QuizBank({ quizzes }) {
           </Box>
           <Box flex="1" width="80%" mx="auto" justifyContent="center" display={"inline-block"} marginTop={"10"}>
             <QuizBankView quizzes={quizzes} />
+          <Box flex="1" width="80%" mx="auto" justifyContent="center" display={"inline-block"}>
+            <QuizTable quizzes={quizzes} />
           </Box>
           <Footer />
         </Box>
