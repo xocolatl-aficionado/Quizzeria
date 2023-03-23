3e0b93dfc127908da42ca206f50c9ae4a21baa57
mehadihn <hi@mehadihassan.com>
2023-03-23 09:34:03 -0230

    Session Added for quizbank page

--- pages/student/quizBank/index.tsx
+++ pages/student/quizBank/index.tsx
@@ -12,16 +12,20 @@ import QuizTable from "../../../components/QuizBankTable";
 import { Box } from "@chakra-ui/react";
 import clientPromise from "../../../src/lib/mongodb";
 
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export async function getServerSideProps() {
   try {
-   
+
     const client = await clientPromise;
     const db = client.db("test");
-    const quiz =  await db.collection("quizes").find({}).toArray();
-   
+    const quiz = await db.collection("quizes").find({}).toArray();
+
     return {
-      props: { 
-        quiz: JSON.parse(JSON.stringify(quiz)) 
+      props: {
+        quiz: JSON.parse(JSON.stringify(quiz))
       },
     };
   } catch (e) {
@@ -35,27 +39,49 @@ export async function getServerSideProps() {
  * @returns 
  */
 export default function QuizBank({ quiz }) {
-  return (
-    <>
-      <Head>
-        <title>Quiz Bank</title>
-        <meta name="description" content="Quiz App Home for students" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="../../../components/styles.css" />
-      </Head>
-      <ChakraProvider>
-        <Box minHeight="100vh" display="flex" flexDirection="column">
-          <NavBar />
-          <Box justifyContent="center">
-            <Card />
-          </Box>
-          <Box flex="1" width="80%" mx="auto" justifyContent="center" display={"inline-block"} marginTop={"10"}>
-            <QuizTable data={quiz}/>
-          </Box>
-          <Footer />
-        </Box>
-      </ChakraProvider>
-    </>
-  );
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  const userRole = (session: any) => {
+    let role = session?.user?.role;
+    if (role) return role;
+    return null;
+  };
+
+  useEffect(() => {
+    if (status === "unauthenticated") router.replace("/");
+  }, [status]);
+
+  if (status === "authenticated") {
+    var role: any = userRole(session);
+
+    if (role == "admin") {
+      router.replace("/admin");
+    } else if (role == "student") {
+
+      return (
+        <>
+          <Head>
+            <title>Quiz Bank</title>
+            <meta name="description" content="Quiz App Home for students" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="stylesheet" href="../../../components/styles.css" />
+          </Head>
+          <ChakraProvider>
+            <Box minHeight="100vh" display="flex" flexDirection="column">
+              <NavBar />
+              <Box justifyContent="center">
+                <Card />
+              </Box>
+              <Box flex="1" width="80%" mx="auto" justifyContent="center" display={"inline-block"} marginTop={"10"}>
+                <QuizTable data={quiz} />
+              </Box>
+              <Footer />
+            </Box>
+          </ChakraProvider>
+        </>
+      );
+    }
+  }
 }
