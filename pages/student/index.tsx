7bab6a8abc81b39f5a09c4250d82b1615463b838
Majid Khan Burki <majidkhanburki1@gmail.com>
2023-03-25 11:32:41 -0230

    Extensive changes in index.ts of Student's Ui
    Made changes to the Database as well to to display marks for a user only and not not all quizzes.
    
    Added functionality for fetching user name from the session as well and passing it on to the server side props function,

--- pages/student/index.tsx
+++ pages/student/index.tsx
@@ -11,59 +11,85 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-export async function getServerSideProps() {
+import { getSession } from 'next-auth/react'
+
+export async function getServerSideProps({ req }) {
   try {
 
-    const { data: session, status } = useSession();
+    const session = await getSession({ req });
+    const dbUser = session?.user?.name;
+
     const client = await clientPromise;
     const db = client.db("test");
 
-    const dbUser = getUserName(session);
+    //const dbUser = getUserName(session);
     const user = await db
       .collection("users")
       .findOne({ "name": dbUser })
 
-    console.log(dbUser)
+      
+
     const quizzes = user?.quizzes;
 
-    const userQuizzes = await db.collection("quizes")
-      .find({})
-      .toArray()
+    const userQuizzes = [];
+    for (const quiz of quizzes) {
+      const tempQuiz =
+      { 
+        quiz: await db
+        .collection('quizes').findOne({_id: quiz.quiz}),
+        marks: quiz.marks
+      }
+      
+      let _id =  tempQuiz.quiz._id.toString();
+      
+      tempQuiz.quiz._id = _id; 
 
-    console.log(quizzes)
+      userQuizzes.push(tempQuiz)
+    } 
+    
     return {
-      props: { quizzes: JSON.parse(JSON.stringify(userQuizzes)) },
+      props: { quizzes: userQuizzes },
     };
   } catch (e) {
     console.error(e);
+    return {
+      props: []
+    };
   }
 }
 
-export default function StudentHome({ quizzes }) {
+type StudentHomePropsInterface = {
+  quizzes: Array<{
+    [key in string]: string;
+  }>;
+};
+
+export default function StudentHome({ quizzes }: StudentHomePropsInterface) {
+
   const router = useRouter();
   const { data: session, status } = useSession();
-  
+
   const userRole = (session: any) => {
     let role = session?.user?.role;
     if (role) return role;
     return null;
   };
 
-  useEffect (() => {
-    if (status === "unauthenticated") router.replace ("/");
-    }, [status]);
-  
-    if (status === "authenticated"){
-      var role: any = userRole(session);
+  useEffect(() => {
+    if (status === "unauthenticated") router.replace("/");
+  }, [status]);
 
-      if (role == "admin") {
-        router.replace("/admin");
-      } else if (role == "student") {
-        
+  if (status === "authenticated") {
+    var role: any = userRole(session);
+
+    if (role == "admin") {
+      router.replace("/admin");
+    } else if (role == "student") {
       return (
         <>
           <Head>
             <title>Quiz App</title>
+
             <meta name="description" content="Quiz App Home for students" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
