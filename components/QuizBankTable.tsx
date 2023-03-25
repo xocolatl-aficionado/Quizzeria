a9d4f8436a72c4ee2b400036acdfd26c90db2475
mehadihn <hi@mehadihassan.com>
2023-03-25 07:18:37 -0230

    Quiz Table Update

--- components/QuizBankTable.tsx
+++ components/QuizBankTable.tsx
@@ -7,6 +7,7 @@ import {Quiz} from "../src/models/studentQuizBank";
  * to load css styles to the page since there are no default pagination in Chakra UI
  */
 import GlobalStyles from "./globalStyles"
+import { Global } from '@emotion/react';
 
 interface Props {
   quizzes: Quiz[];
@@ -22,69 +23,17 @@ const QuizTable =  (quizItem) => {
 
   const ITEMS_PER_PAGE = 5;
   const PAGE_COUNT = Math.ceil(quizzes.length / ITEMS_PER_PAGE)
+  const offset = currentPage * ITEMS_PER_PAGE;
+  const pagedQuizzes = quizzes.slice(offset, offset + ITEMS_PER_PAGE);
 
   const handlePageClick = ({ selected }: { selected: number }) => {
     setCurrentPage(selected);
   };
-  const offset = currentPage * ITEMS_PER_PAGE;
   
-  const pagedQuizzes = quizzes.slice(offset, offset + ITEMS_PER_PAGE);
 
   return (
     <>
-    <Global
-        styles={`
-          .pagination {
-            display: flex;
-            justify-content: center;
-            margin-top: 20px;
-            pointer-events: auto;
-            cursor: pointer;
-            list-style:none;s
-          }
-          
-          ::marker {
-            list-style: none;
-            display:none;
-          }
-
-          .pagination li a{
-            display: inline-block;
-            margin: 0 10px;
-            border: 1px solid orange;
-            padding: 10px;
-            border-radius:5px;
-            background-color:#FFFFCE;
-            cursor: pointer;
-            pointer-events: auto;
-            font-weight:bold;
-          }
-
-
-          .pagination li a.active{
-            color: black;
-            bg-color:orange;
-          }
-
-          .pagination li.hover{
-            background-color:orange;
-          }
-
-          .pagination li.clicked{
-            background-color:orange;
-            color:orange;
-          }
-
-          .pagination__link--active {
-            color: #DD6B20;
-          }
-
-          .pagination__link--disabled {
-            opacity: 0.5;
-            pointer-events: none;
-          }
-        `}
-      />
+    <GlobalStyles/>
 
       <Table variant="striped">
         <Thead>
