3d0b665cc5688121b77edce90224a9923b143dde
mehadihn <hi@mehadihassan.com>
2023-03-28 14:07:09 -0230

    Password Validation Class and Test Casr

---
+++ src/business/validation/PasswordCheck/passCheck.js
@@ -0,0 +1,47 @@
+export default class passWordCheck {
+    constructor(password) {
+      this.password = password;
+    }
+    
+    checkCases() {
+      // Check length
+      let passCases = true;
+      let errorMessage = '';
+      if (this.password.length < 8) {
+        errorMessage += 'The password must contain at least 8 characters\n';
+        passCases = false;
+      }
+      // Check Only Letters value
+      if (/^[a-zA-Z]+$/.test(this.password) == true) {
+        errorMessage += 'The password cannot contain only letters\n';
+        passCases = false;
+      }
+
+      // Check Only number value
+      if (/^\d+$/.test(this.password) == true) {
+        errorMessage += 'The password cannot contain only numbers\n';
+        passCases = false;
+      }
+
+      // Check numeric value
+      if (/\d/.test(this.password) !== true) {
+        errorMessage += 'The password must contain at least 1 numeric value\n';
+        passCases = false;
+      }
+      
+      // Check special characters
+      if (/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(this.password) !== true) {
+        errorMessage += 'The password must contain at least 1 special character\n';
+        passCases = false;
+      }
+  
+      return { passCases, errorMessage };
+    }
+  }
+  
+  // let password = new passWordCheck("abcd");
+  // console.log("Password Given: " , password)
+  // const { passCases, errorMessage } = password.checkCases();
+  // console.log(passCases);
+  // console.log(errorMessage);
+  
