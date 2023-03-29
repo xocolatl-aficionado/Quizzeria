5ae8393d83e1894783b58f76b0ff7f48b3aaf9dd
Majid Khan Burki <majidkhanburki1@gmail.com>
2023-03-29 10:45:36 -0230

    Question interface
    Create question interface

---
+++ src/business/models/Question.ts
@@ -0,0 +1,13 @@
+/**
+ * Contains fields pertaining to all kinds of questions
+ */
+export default interface Question {
+    qid: number;
+    question: string;
+    answer:string;
+    subject:string;
+    type:string;
+    
+  }
+
+  
