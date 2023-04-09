0197db213940127f361088f89849223c9f2bcb39
mehadihn <hi@mehadihassan.com>
2023-04-09 15:52:42 -0230

    Removed console log

--- pages/api/admin/deleteQuiz.js
+++ pages/api/admin/deleteQuiz.js
@@ -5,7 +5,6 @@ export default async (req, res) => {
 
   try {
     var qd = new MongoQuizData();
-    console.log(req.body.subject)
     const deleteQuiz = await qd.deleteQuiz(req.body.subject)
     const setQuesSubNull = await qd.setQuestionSubNull(req.body.subject)
     res.status(200).json();
