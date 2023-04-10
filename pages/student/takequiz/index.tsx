a5a8a069663c858736cd09a41dd590296e42867e
Heshani2023 <hhettiarachc@mun.ca>
2023-04-10 16:13:42 -0230

    Linked Take a quiz UI with take quiz button in quizbank
    -Passed subject as a parameter for the take a quiz ui to filter the questions according to subject.
    -redirected take quiz button to take quiz ui
    -added subject as the title of the page
    - displayed subject as the quiz heading

--- pages/student/takequiz/index.tsx
+++ pages/student/takequiz/index.tsx
@@ -2,9 +2,8 @@ import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "../../../components/studentNavbar";
 import Footer from "../../../components/Footer";
-import Card from "../../../components/ExamTitleCard";
 import ShortAnswerQuestionList from "../../../components/shortAnswerQuestions";
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, chakra } from "@chakra-ui/react";
 import MongoQuizData from "../../../src/data/dbconnection";
 import Question from "../../../src/business/models/question";
 import { useRouter } from "next/router";
@@ -13,17 +12,21 @@ import { useSession } from "next-auth/react";
 
 interface TakeAQuizProps {
   questions: Array<Question>;
+  subjectValue: string;
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
+  const { subject } = context.query;
+  const subjectValue = subject ? subject : '';
+  console.log("subject is: ",subjectValue);
   try {
-
     var qd = new MongoQuizData();
-    let questions = await qd.findQuestionListOfAQuiz("Math");
+    let questions = await qd.findQuestionListOfAQuiz(subjectValue);
     console.log("the rerurned array of questions: ",questions);
     return {
       props: {
-        questions: JSON.parse(JSON.stringify(questions))
+        questions: JSON.parse(JSON.stringify(questions)),
+        subjectValue
       },
     };
   } 
@@ -39,7 +42,7 @@ export async function getServerSideProps() {
  * Function to return Take a Quiz UI for students
  * @returns Attempt a Quiz UI for students
  */
-export default function takeAQuiz({ questions }: TakeAQuizProps) {
+export default function takeAQuiz({ questions,subjectValue}: TakeAQuizProps) {
   const router = useRouter();
   const { data: session, status } = useSession();
 
@@ -60,7 +63,7 @@ export default function takeAQuiz({ questions }: TakeAQuizProps) {
       return (
         <>
           <Head>
-            <title>Quiz Bank</title>
+            <title>{subjectValue}</title>
             <meta name="description" content="Quiz App Home for students" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
@@ -68,11 +71,12 @@ export default function takeAQuiz({ questions }: TakeAQuizProps) {
           <ChakraProvider>
             <Box minHeight="100vh" display="flex" flexDirection="column">
               <NavBar />
-              <Box justifyContent="center">
-                <Card />
+              <Box textAlign={"center"} width={"100%"} margin={"auto"} paddingTop={'2vh'}>
+                <Heading fontSize={'4xl'}>{subjectValue}</Heading>
+                <chakra.h3 fontSize={'xl'} paddingTop={'1vh'}>55:30</chakra.h3>
               </Box>
               <Box flex="1" mx="auto" justifyContent="center" alignContent={"center"}>
-                <ShortAnswerQuestionList questions={questions} />
+                <ShortAnswerQuestionList questions={questions}/>
               </Box>
               <Footer />
             </Box>
