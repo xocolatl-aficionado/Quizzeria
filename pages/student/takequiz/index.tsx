4952fce402e21797177f4f51d7ea781052780565
Heshani2023 <hhettiarachc@mun.ca>
2023-04-10 10:23:25 -0230

    Added user  session to the page

--- pages/student/takequiz/index.tsx
+++ pages/student/takequiz/index.tsx
@@ -7,19 +7,20 @@ import ShortAnswerQuestionList from "../../../components/shortAnswerQuestions";
 import { Box } from "@chakra-ui/react";
 import MongoQuizData from "../../../src/data/dbconnection";
 import Question from "../../../src/business/models/question";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
 
 interface TakeAQuizProps {
   questions: Array<Question>;
 }
-/**
- * To fetch questionlist from the data layer
- * @returns JSON string of questions for Math subject as received from the database
- */
+
 export async function getServerSideProps() {
   try {
 
     var qd = new MongoQuizData();
     let questions = await qd.findQuestionListOfAQuiz("Math");
+    console.log("the rerurned array of questions: ",questions);
     return {
       props: {
         questions: JSON.parse(JSON.stringify(questions))
@@ -36,30 +37,48 @@ export async function getServerSideProps() {
 }
 /**
  * Function to return Take a Quiz UI for students
- * @param questions - json string of questions data for Math subject as received from the database.
  * @returns Attempt a Quiz UI for students
  */
 export default function takeAQuiz({ questions }: TakeAQuizProps) {
-  return (
-    <>
-      <Head>
-        <title>Take A Quiz</title>
-        <meta name="description" content="Take a quiz screen for students" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <ChakraProvider>
-        <Box minHeight="100vh" display="flex" flexDirection="column">
-          <NavBar />
-          <Box justifyContent="center">
-            <Card />
-          </Box>
-          <Box flex="1" mx="auto" justifyContent="center" alignContent={"center"}>
-            <ShortAnswerQuestionList questions={questions}/>
-          </Box>
-          <Footer />
-        </Box>
-      </ChakraProvider>
-    </>
-  );
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  const userRole = (session: any) => {
+    let role = session?.user?.role;
+    if (role) return role;
+    return null;
+  };
+  useEffect(() => {
+    if (status === "unauthenticated") router.replace("/");
+  }, [status]);
+  if (status === "authenticated") {
+    var role: any = userRole(session);
+
+    if (role == "admin") {
+      router.replace("/admin");
+    } else if (role == "student") {
+      return (
+        <>
+          <Head>
+            <title>Quiz Bank</title>
+            <meta name="description" content="Quiz App Home for students" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <ChakraProvider>
+            <Box minHeight="100vh" display="flex" flexDirection="column">
+              <NavBar />
+              <Box justifyContent="center">
+                <Card />
+              </Box>
+              <Box flex="1" mx="auto" justifyContent="center" alignContent={"center"}>
+                <ShortAnswerQuestionList questions={questions} />
+              </Box>
+              <Footer />
+            </Box>
+          </ChakraProvider>
+        </>
+      );
+    }
+  }
 }
