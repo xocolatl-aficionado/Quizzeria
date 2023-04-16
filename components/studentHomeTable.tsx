23b5e0c6acf940433daadbb85e19406ff4eedc9d
Majid Khan Burki <majidkhanburki1@gmail.com>
2023-04-16 17:56:00 -0230

    Updating user marks functionality added to StudentHome Component

--- components/studentHomeTable.tsx
+++ components/studentHomeTable.tsx
@@ -1,6 +1,9 @@
 /**
- * Designing a table view for students to display their current quizzes
- */
+
+A React component that renders a table of quizzes.
+@param {Array<UserQuiz>} quizzes - An array of quizzes to display in the table.
+*/
+
 import {
   Table,
   Thead,
@@ -12,6 +15,10 @@ import {
   Center,
 } from "@chakra-ui/react";
 import Quiz, { UserQuiz } from "../src/business/models/Quiz";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+
 
 /**
  * Interface props to define the Quiz
@@ -20,37 +27,56 @@ interface QuizProps {
   quizzes: Array<UserQuiz>;
 }
 
-export async function updateMarks(postId: number, marks: number) {
-  try {
-    const options: any = {
-      method: "POST",
-
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-
-      body: `{"marks": ${marks}}`,
-    };
-
-    let response = await fetch(`/api/quizes/post?id=${postId}`, options)
-      .then((response) => response.json())
-      .then((response) => console.log(response))
-      .catch((err) => console.error(err));
-
-    window.location.reload();
-  } catch (error) {
-    console.log("An error occured while updating marks ", error);
-  }
-}
-
-/**
- * To define the table and map it with the quiz data object
- * @param param0 : Quiz object created with the types for loading data
- * @returns a table of quiz data related to the student/quiztaker
- */
-
 export default function QuizTable({ quizzes }: QuizProps) {
+
+  // Router object
+  const router = useRouter();
+
+  // Session object
+  const session = useSession();
+
+  // Extracting user's email from the session
+  var myEmail = session.data?.user?.email;
+
+
+  /**
+  
+  An async function that updates the marks of a quiz in the database.
+  
+  @param {string} email - The email address of the user taking the quiz.
+  
+  @param {string} subject - The subject of the quiz.
+  
+  @param {number} marks - The marks obtained by the user in the quiz.
+  */
+
+  const updateMarks = async (email: string, subject: string, marks: number) => {
+    try {
+
+
+      const options: any = {
+        method: "POST",
+
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({ subject: `${subject}`, marks: `${marks}` }),
+      };
+
+      let response = await fetch(`/api/quizes/post?email=${email}`, options)
+        .then((response) => response.json())
+        .then((response) => console.log(response))
+        .catch((err) => console.error(err));
+
+      router.replace("/student/takequiz?subject=" + subject)
+
+    } catch (error) {
+      console.log("An error occured while updating marks ", error);
+    }
+  }
+
 
   return (
     <Table variant="striped">
@@ -73,7 +99,7 @@ export default function QuizTable({ quizzes }: QuizProps) {
             <Td textAlign={"right"}>
               <Button
                 colorScheme="orange"
-                onClick={() => updateMarks(userQuiz._id as Number, -1)}
+                onClick={() => updateMarks(myEmail ?? "", userQuiz.subject, -1)}
               >
                 Retake Quiz
               </Button>
